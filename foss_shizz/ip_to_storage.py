9fe30c0e3fc175c2f8fd392e386440a1ddbd3062
David Adams <dadams0641@gmail.com>
2024-11-14 19:26:17 -0500

    fixup! Add SA IP Manipulation Script

--- foss_shizz/ip_to_storage.py
+++ foss_shizz/ip_to_storage.py
@@ -17,7 +17,9 @@ currdate=current_date.strftime("%Y-%m-%d")
 currdate_split=currdate.split('-')
 tfvars=sys.argv[1]
 rg_name=sys.argv[2]
-add_remove = sys.argv[3]
+add_remove=sys.argv[3]
+infra_resource_group_name=sys.argv[4]
+infra_storage_account_name=sys.argv[5]
 ip_address= subprocess.Popen("curl -s icanhazip.com", shell=True, stdout=subprocess.PIPE).stdout.read().decode().rstrip()
 
 #############################################################################################################################################################################################################################################################################################################################
@@ -46,6 +48,7 @@ def add_remove_storage_accounts():
     count=0
     storage_names={}
     saf=False
+    os.system("az storage account network-rule " + add_remove + " --resource-group " + infra_resource_group_name + " --account-name " + infra_storage_account_name + " --ip-address " + str(ip_address))
     sa_find=subprocess.Popen('az storage account list --resource-group ' + rg_name + ' --query "[].name" --output tsv 2>/dev/null', shell=True, stdout=subprocess.PIPE)
     for saf in sa_find.stdout:
         saf = saf.decode().rstrip()
@@ -56,7 +59,7 @@ def add_remove_storage_accounts():
             sys.exit(0)
         storage_names["Name"]=saf
         storage_name=json.dumps(storage_names)
-        print("az storage account network-rule " + add_remove + " --resource-group " + rg_name + " --account-name " + saf + " --ip-address " + str(ip_address))
+        os.system("az storage account network-rule " + add_remove + " --resource-group " + rg_name + " --account-name " + saf + " --ip-address " + str(ip_address))
 
 add_remove_storage_accounts()
 json_file.close()
